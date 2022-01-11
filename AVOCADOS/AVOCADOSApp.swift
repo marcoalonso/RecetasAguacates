4fe78326d32968a5f92fdd8343d8665c1f8c8510
marcoalonso <marcoalonsorota@hotmail.com>
2022-01-11 09:44:03 -0600

    Empezando

---
+++ AVOCADOS/AVOCADOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVOCADOSApp.swift
+//  AVOCADOS
+//
+//  Created by marco rodriguez on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AVOCADOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
